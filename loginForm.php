6ef6962b33211ed7a0c331cbeb01e583e12fde81
Maximiliano Zapata <maxzap@users.noreply.github.com>
2017-09-06 17:32:26 -0300

    Modifico el loginForm.php

--- loginForm.php
+++ loginForm.php
@@ -2,7 +2,10 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Courgette|Anton|Baloo+Tamma|Marvel|Katibeh" rel="stylesheet">
     <title>Login | Borrowin!</title>
     <link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
     <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Courgette|Anton|Baloo+Tamma|Marvel|Katibeh" rel="stylesheet">
@@ -15,5 +18,90 @@
       <input type="password" name="password" placeholder="Tu contraseña..">
       <button type="submit" name="button">Ingresar</button>
     </form>
+    <section class="contact-us">
+      <div class="container">
+        <div class="row">
+          <div class="fields">
+              <form role="form">
+                        <div class="panel-title text-center">
+                            <h1 class="title">Sign Up</h1>
+                          </div>
+                          <br>
+                  <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <label> Your  Name </label> <br>
+                    <div class="input-group input-group-lg">
+                      <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-user fa" aria-hidden="true"></i></span>
+                      <input type="text" class="form-control" placeholder="Your Name" aria-describedby="sizing-addon1">
+                    </div>
+                    <br>
+                    <label> Your E-mail </label> <br>
+                    <div class="input-group input-group-lg">
+                      <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-envelope fa" aria-hidden="true"></i></span>
+                      <input type="text" class="form-control" placeholder="Your Email" aria-describedby="sizing-addon1">
+                    </div>
+                    <br>
+                    <label> Your User Name </label> <br>
+                    <div class="input-group input-group-lg">
+                      <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-users fa" aria-hidden="true"></i></span>
+                      <input type="text" class="form-control" placeholder="Your User Name" aria-describedby="sizing-addon1">
+                    </div>
+                    <br>
+                    <label> Password </label> <br>
+                    <div class="input-group input-group-lg">
+                      <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                      <input type="password" class="form-control" placeholder="Enter Your Password" aria-describedby="sizing-addon1">
+                       <span class="input-group-btn btnnn">
+                    <button class=" btn btn-default" type="button">Show</button>
+                  </span>
+                    </div>
+                    <br>
+                    <label> Password confirmation </label> <br>
+                    <div class="input-group input-group-lg">
+                      <span class="input-group-addon" id="sizing-addon1"><i class="fa fa-lock fa-lg" aria-hidden="true"></i></span>
+                      <input type="password" class="form-control" placeholder="Confirm Your Password" aria-describedby="sizing-addon1">
+                       <span class="input-group-btn btnnn">
+                    <button class=" btn btn-default" type="button">Show</button>
+                  </span>
+                    </div>
+                    <br>
+                  <label>  Are You ? </label> <br>
+                  <input type="radio" name="female" placeholder="Gender"> Male <br>
+                    <input type="radio" name="female" placeholder="Gender"> Female<br>
+                    <input type="checkbox" name="agree">  I Agree To The Terms & Conditions <br>
+                          <br>
+                  <button class="btn btn-primary btn-lg btn-block"> Register </button>
+                    <br>
+                  <a href="">Login</a>
+                  </div>
+              </form>
+
+
+          </div>
+        </div>
+      </div>
+
+    </section>
+
+    <!-- [if it ie 9] -->
+
+    <script src="js/html5shiv.min.js"></script>
+    <!-- [endif] -->
+
+    <!-- jquery file -->
+    <script src="js/jquery-3.1.1.min.js"></script>
+
+
+    <!-- Bootstrap file -->
+    <script src="js/bootstrap.min.js"></script>
+
+    <!-- main js file  -->
+    <script src="js/main.js"> </script>
+
+    <!-- Start WOW  -->
+    <script src="js/wow.min.js"> </script>
+     <script> new WOW().init(); </script>
+    <!-- End WOW  -->
+
+
   </body>
 </html>
